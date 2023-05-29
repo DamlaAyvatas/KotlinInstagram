417c8f7a15e8b341bbd17e41164799d142b830eb
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-05-30 00:31:11 +0300

    main activity ui

--- app/src/main/java/com/dayvatas/kotlininstagram/UploadActivity.kt
+++ app/src/main/java/com/dayvatas/kotlininstagram/UploadActivity.kt
@@ -2,20 +2,10 @@ package com.dayvatas.kotlininstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class UploadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-    }
-
-    fun signInClicked(view : View){
-
-    }
-
-    fun signUpClicked(view : View){
-
+        setContentView(R.layout.activity_upload)
     }
 }
