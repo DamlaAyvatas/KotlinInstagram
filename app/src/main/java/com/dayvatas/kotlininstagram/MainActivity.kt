f188a31d69f388ff18fa50bd4cd0cfef35c23448
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-06-01 21:46:18 +0300

    import firebase authentication

--- app/src/main/java/com/dayvatas/kotlininstagram/MainActivity.kt
+++ app/src/main/java/com/dayvatas/kotlininstagram/MainActivity.kt
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.dayvatas.kotlininstagram.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
+        auth = Firebase.auth
     }
 
     fun signInClicked(view : View){
