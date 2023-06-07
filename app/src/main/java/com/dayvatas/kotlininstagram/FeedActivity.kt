2d5d85bd5963ce365de94163411465efb4f4e63c
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-06-07 23:30:14 +0300

    menu on feed activity

--- app/src/main/java/com/dayvatas/kotlininstagram/FeedActivity.kt
+++ app/src/main/java/com/dayvatas/kotlininstagram/FeedActivity.kt
@@ -2,14 +2,33 @@ package com.dayvatas.kotlininstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.dayvatas.kotlininstagram.databinding.ActivityFeedBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class FeedActivity : AppCompatActivity() {
     private lateinit var binding : ActivityFeedBinding
+    lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFeedBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        auth = Firebase.auth
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.insta_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return super.onOptionsItemSelected(item)
     }
 }
