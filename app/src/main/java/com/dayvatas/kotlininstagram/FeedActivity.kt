5155591f49b3be448d31793968d690704a71e225
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-06-12 23:58:46 +0300

    take data on feed activity

--- app/src/main/java/com/dayvatas/kotlininstagram/FeedActivity.kt
+++ app/src/main/java/com/dayvatas/kotlininstagram/FeedActivity.kt
@@ -5,20 +5,41 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.dayvatas.kotlininstagram.databinding.ActivityFeedBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class FeedActivity : AppCompatActivity() {
     private lateinit var binding : ActivityFeedBinding
     lateinit var auth : FirebaseAuth
+    private lateinit var db : FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFeedBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
         auth = Firebase.auth
+        getData()
+    }
+
+    private fun getData (){
+        db.collection("Posts").addSnapshotListener{value, error ->
+            if(error != null){
+                Toast.makeText(this,error.localizedMessage,Toast.LENGTH_LONG).show()
+            }else{
+                if(value != null){
+                    if(!value.isEmpty){
+                        val documents = value.documents         //dokümanlarımı bir listede aldım
+                        for(document in documents){
+
+                        }
+                    }
+                }
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
