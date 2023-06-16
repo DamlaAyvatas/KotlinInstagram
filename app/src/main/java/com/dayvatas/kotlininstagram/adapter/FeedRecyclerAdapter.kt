7e6bb30461975d830d3270d0c5adaf462769a51a
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-06-16 23:54:41 +0300

    adapter -> onCreateViewHolder

--- app/src/main/java/com/dayvatas/kotlininstagram/adapter/FeedRecyclerAdapter.kt
+++ app/src/main/java/com/dayvatas/kotlininstagram/adapter/FeedRecyclerAdapter.kt
@@ -1,4 +1,29 @@
 package com.dayvatas.kotlininstagram.adapter
 
-class FeedRecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import com.dayvatas.kotlininstagram.databinding.RecyclerRowBinding
+import com.dayvatas.kotlininstagram.model.Post
+
+class FeedRecyclerAdapter (val postArrayList : ArrayList<Post>) :
+    RecyclerView.Adapter<FeedRecyclerAdapter.FeedHolder>() {
+    class FeedHolder(val binding : RecyclerRowBinding) : RecyclerView.ViewHolder(binding.root){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedHolder {
+        //recyclerRow ile yani layout ile burada bağlanacak
+        val binding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return FeedHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: FeedHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
