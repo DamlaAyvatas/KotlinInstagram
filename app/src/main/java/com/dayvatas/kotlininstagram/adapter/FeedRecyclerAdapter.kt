38cb5b3e795d40ba444b5046ddde48983c1889c6
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-06-18 21:42:44 +0300

    add picasso for image downloading

--- app/src/main/java/com/dayvatas/kotlininstagram/adapter/FeedRecyclerAdapter.kt
+++ app/src/main/java/com/dayvatas/kotlininstagram/adapter/FeedRecyclerAdapter.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Recycler
 import com.dayvatas.kotlininstagram.databinding.RecyclerRowBinding
 import com.dayvatas.kotlininstagram.model.Post
+import com.squareup.picasso.Picasso
 
 class FeedRecyclerAdapter (private val postList : ArrayList<Post>) :
     RecyclerView.Adapter<FeedRecyclerAdapter.FeedHolder>() {
@@ -22,7 +23,7 @@ class FeedRecyclerAdapter (private val postList : ArrayList<Post>) :
     override fun onBindViewHolder(holder: FeedHolder, position: Int) {
         holder.binding.recyclerEmailText.text = postList.get(position).email
         holder.binding.recyclerCommentText.text = postList.get(position).comment
-
+        Picasso.get().load(postList.get(position).downloadUrl).into(holder.binding.recyclerImageView)
 
     }
     override fun getItemCount(): Int {
